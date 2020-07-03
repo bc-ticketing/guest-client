458b82b43d21300027ed0808c134ea643ed0463f
Michael Bucher <michael.bucher2@uzh.ch>
2020-07-03 15:49:40 +0200

    #16: Added empty Event view and SearchBar component

---
+++ src/views/Event.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="event">
+    <div class="container-fluid"></div>
+    <div class="container">
+      <div class="return-arrow">
+        <router-link to="/events">go back</router-link>
+      </div>
+      <p>event detail page</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Event"
+};
+</script>
+
+<style scoped></style>
