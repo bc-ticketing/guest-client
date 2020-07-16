6c5697bdb2b52fbcb601e43bedca283c070a0f6a
Claudio Brasser <clados@pop-os.localdomain>
2020-07-16 13:38:35 +0200

    mobile wallet linking started #16

--- src/views/WalletFunding.vue
+++ src/views/WalletFunding.vue
@@ -1,15 +1,35 @@
 <template>
   <div>
       <h1>Wallet Funding</h1>
-      <p>Swapping token and funding the Ethereum wallet with external payment processor (ideally with credit card)</p>
+      <div class='metamask-info'>
+   <p>Network: {{ networkName }}</p>
+   <p>Account: {{ web3.account}}</p>
+   <p>Balance: {{ web3.balance }}</p>
+ </div>
   </div>
 </template>
 
 <script>
+
+import {NETWORKS} from './../util/constants/constants.js'
+
 export default {
   name:'WalletFunding',
   components:{
-  }
+  },
+  beforeCreate () {
+    console.log('registerWeb3 Action dispatched from casino-dapp.vue')
+    this.$store.dispatch('registerWeb3')
+    
+  },
+  computed: {
+   web3 () {
+     return this.$store.state.web3
+     },
+    networkName () {
+      return NETWORKS[this.web3.networkId]
+    }
+   }
 }
 </script>
 
