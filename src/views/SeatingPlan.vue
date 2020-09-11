af145339139f0b39538e599e347ba181b9e1a6f7
Claudio <claudio@pop-os.localdomain>
2020-09-11 09:29:22 +0200

    seating plan grid

--- src/views/SeatingPlan.vue
+++ src/views/SeatingPlan.vue
@@ -1,48 +1,261 @@
 <template>
-  <div class="seating-container" :ref='"cont"' @mouseup="disableMouseDown" @mousedown="enableMouseDown">
-      <div class="col" v-for="col in cols" v-bind:key="'col_'+col">
-          <div :ref='"seat_"+col+"_"+row' @mouseenter="mouseEnter(col, row)" @click="selectSeat(col, row)" class="seat" v-for="row in rows" v-bind:key="'seat_'+row"> </div>
+<div class="wrapper">
+    <div class="settings">
+        <div class="menu-group">
+            <h3>Venue Size</h3>
+      <label for="rows">Rows:</label>
+      <input type="number" name="rows" id="rows" v-model="rows">
+        <label for="cols">Cols:</label>
+      <input type="number" name="cols" id="cols" v-model="cols">
+      
       </div>
+                  <h3>Create new Ticket Type and Seating Plan</h3>
+
+      <div class="menu-group">
+            <label for="fungible">Fungible</label>
+            <input type="radio" name="fungible" value=1 v-model.number="fungible">
+            <label for="nonfungible">Non Fungible</label>
+            <input type="radio" name="nonfungible" value=0 v-model.number="fungible">
+      </div>
+      <div class="menu-group" v-show="isFungible">
+        <label for="nrTickets">Number of fungible tickets</label>
+        <input type="text" name="nrFungible" v-model="nrFungible">
+      </div>
+      <div class="menu-group">
+            <label for="name">Name</label>
+            <input type="text" v-model="ticketName" placeholder="Ticket Category Name">
+        </div>
+        <div class="menu-group">
+            <label for="price">Price</label>
+            <input type="number" name="number" v-model="ticketPrice">
+      </div>
+      
+  </div>
+  <div class="seating-container" :ref='"cont"' @mouseup="disableMouseDown" @mousedown="enableMouseDown" @mouseleave="leaveGrid" draggable="false">
+      <div class="col" v-for="col in cols" v-bind:key="'col_'+col">
+          <div :ref='"seat_"+col+"_"+row' data-status='free' @mousedown="mouseDownOnTile(col, row)" @mouseup="mouseUpOnTile(col, row)" @mouseenter="mouseEnter(col, row)" @click="selectSeat(col, row)" class="seat" v-for="row in rows" v-bind:key="'seat_'+row"> </div>
+      </div>
+  </div>
+  <div class="design-settings">
+      <div class="section">
+ <div class="menu-group">
+                    <h3>Design Options</h3>
+
+          <label for="color-selction">Color</label>
+          <input class="radio-color yellow" type="radio" name='color-1' v-model="color" value="#D08770">
+          <input class="radio-color orange" type="radio" name='color-2' v-model="color" value="#EBCB8B">
+          <input class="radio-color red" type="radio" name='color-3' v-model="color" value="#bf616a">
+      </div>
+      <div class="menu-group">
+          <label for="block-selection">Block selection</label>
+          <input type="checkbox" v-model="block_selection" value=true>
+      </div>
+      <div class="menu-group">
+          <button class='btn btn-danger' @click="resetAll">Reset</button>
+          <button class='btn btn-success' @click="submit">Submit</button>
+      </div>
+      </div>
+      <div class="section legend">
+          <h3>Legend</h3>
+          <div class="menu-group">
+              <div class="seat free"></div>
+              <span>Free</span>
+          </div>
+      </div>
+     
+  </div>
+
+  
   </div>
 </template>
 
 <script>
-
+/* This view is a demo for our seating plan generator, which will be used in the host client to let an event host generate somewhat accurate, yet arbitrary, seating plans for their venue. 
+    The code will also be adapted and used in the guest client for displaying which seats are available for purchase to a customer.
+ */
 
 export default {
   name: "Event",
   data() {
     return {
+        nrFungible: 0,
+        ticketPrice: 0,
+        ticketName: '',
+        color: '#B48EAD',
+        emptyColor: '#8fbcbb',
+        occupiedColor: '#4c566a',
+        stageColor: '#d8dee9',
+        block_selection: false,
+        selection_start: {x: 0,y: 0},
+        fungible: true,
         rows: 12,
-        cols: 5,
+        cols: 12,
         selected_seats: [],
+        last_selected: {x: 0, y: 0},
         mouseDown: false,
+        seatingObject: {
+            rows:0,
+            cols:0,
+            assignedSeats: [],
+        }
     };
   },
   props: {},
-  computed: {
-   
+    computed: { 
+        isFungible() {
+            return this.fungible;
+        }
   },
   watch: {
+      rows: function(val) {
+          this.rows = Number(val);
+          this.updateGridSize();
+      },
+      cols: function(val) {
+          this.cols = Number(val);
+          this.updateGridSize();
+      }
     },
   methods: {
+      buildJson() {
+          this.seatingObject.rows = this.rows;
+          this.seatingObject.cols = this.cols;
+            for (let i = 1; i <= this.cols; i++) {
+                for (let j = 1; j <= this.rows; j++) {
+                    var seat = this.$refs[`seat_${col}_${row}`];
+
+                }
+            }
+
+      },
+      updateGridSize() {
+          this.$refs['cont'].style.gridTemplateColumns = `repeat(${this.cols}, 1fr)`;
+            this.$refs['cont'].style.gridTemplateRows = `repeat(${this.rows}, 20px)`;
+      },
       disableMouseDown() {
-          this.mouseDown = false;
+          this.mouseUpOnTile(this.last_selected.x, this.last_selected.y);
       },
-      enableMouseDown() {
+      enableMouseDown(event) {
+          event.preventDefault();
           this.mouseDown = true;
+          
       },
+      mouseDownOnTile(col, row) {
+          this.mouseDown = true;
+          if(this.block_selection) {
+              this.selection_start = {x: col, y: row};
+          } else {
+            this.selectSeat(col, row);
+          }
+      },
+      mouseUpOnTile(col, row ){
+          this.mouseDown = false;
+          if(this.block_selection) {
+              let start_x = this.selection_start.x < col ? this.selection_start.x : col;
+                let start_y = this.selection_start.y < row ? this.selection_start.y : row;
+                let end_x = this.selection_start.x > col ? this.selection_start.x : col;
+                let end_y = this.selection_start.y > row ? this.selection_start.y : row;
+               for(let i = start_x; i<= end_x; i++) {
+                  for (let j = start_y; j <= end_y; j++) {
+                      this.selectSeat(i,j);
+                  }
+              }
+              this.removeAllTempSelections();
+          }
+          },
         mouseEnter(col, row) {
             if(this.mouseDown) {
-                this.selectSeat(col, row);
+                if(this.block_selection) {
+                    let start_x = this.selection_start.x < col ? this.selection_start.x : col;
+                    let start_y = this.selection_start.y < row ? this.selection_start.y : row;
+                    let end_x = this.selection_start.x > col ? this.selection_start.x : col;
+                    let end_y = this.selection_start.y > row ? this.selection_start.y : row;
+                    this.removeAllTempSelections();
+                    for(let i = start_x; i<= end_x; i++) {
+                        for (let j = start_y; j <= end_y; j++) {
+                            this.markForSelection(i,j);
+                        }
+                    }
+                } else {
+                    this.selectSeat(col, row);
+                }
             }
+            
         },
       selectSeat(col, row) {
-                    var seat = this.$refs[`seat_${col}_${row}`];
-          seat[0].classList.add('selected');
+            var seat = this.$refs[`seat_${col}_${row}`];
+            if(seat[0].dataset.status != 'occupied') {
+          seat[0].dataset.status = 'selected';
+          if(this.fungible) {
+              seat[0].classList.add('fungible');
+          }
+          seat[0].style.backgroundColor = this.color;
+            }
+
+
+          this.last_selected = {x:col, y: row};
+      },
+      markForSelection(col, row) {
+            var seat = this.$refs[`seat_${col}_${row}`];
+            if(seat[0].dataset.status != 'occupied') {
+                seat[0].classList.add('temporary-selected');
+            }
+          
+          this.last_selected = {x:col, y: row};
+      },
+      removeAllTempSelections() {
+          for (let i = 1; i <= this.cols; i++) {
+              for (let j = 1; j <= this.rows; j++) {
+                var seat = this.$refs[`seat_${i}_${j}`];
+                seat[0].classList.remove('temporary-selected');
+                //this.last_selected = {x:col, y: row};
+              }
+              
+          }
+      },
+      leaveGrid() {
+          this.mouseDown = false;
+          if(this.block_selection) {
+              this.mouseUpOnTile(this.last_selected.x, this.last_selected.y);
+          }
       },
       getSeatStatus(col, row) {
-          
+            var seat = this.$refs[`seat_${col}_${row}`];
+            return seat[0].dataset.status;
+      },
+      resetAll() {
+        for (let i = 1; i <= this.cols; i++) {
+            for (let j = 1; j <= this.rows; j++) {
+                var seat = this.$refs[`seat_${i}_${j}`];
+                if( this.getSeatStatus(i,j) != 'occupied'){
+                    seat[0].dataset.status = 'free';
+                    seat[0].style.backgroundColor = this.emptyColor;
+                }
+              }
+              
+          }
+      },
+      submit() {
+          let selected_seats = [];
+        for (let i = 1; i <= this.cols; i++) {
+            for (let j = 1; j <= this.rows; j++) {
+                var seat = this.$refs[`seat_${i}_${j}`];
+                    if(seat[0].dataset.status == 'selected') {
+                        seat[0].dataset.status = 'occupied';
+                        seat[0].style.backgroundColor = this.occupiedColor;
+                        selected_seats.push(`${x}/${y}`);
+                    }
+
+                //this.last_selected = {x:col, y: row};
+              }
+              
+          }
+          /*
+          selected_seats is a list of x/y coordinates. it contains all seats in the venue that have been marked for the ticket to be created
+          for NF tickets: create 1 ticket per selected seat, store on ipfs for each ticket: the x/y index in the grid, the ticket address itself
+          for F tickets: the host can select how many tickets should be created for the selected standing area and the ticket type. Store on IPFS: list of all indices of the seats (for frontend display on guest client).  
+           */
+          //TODO: call SC to create tickets + store metadata on ipfs including ticket mapping
       },
    
   },
@@ -56,23 +269,67 @@ export default {
 </script>
 
 <style scoped>
+.btn {
+    padding: 0.75rem 1.5rem;
+    border: none;
+    margin-right: 2rem;
+    margin-top: 1rem;
+    cursor: pointer;
+}
+.btn-danger {
+    background-color: #bf616a;
+}
+.btn-success {
+    background-color: #a3be8c;
+}
+.radio-color.red{
+    background-color: #bf616a;
+}
+.radio-color.yellow{
+    background-color: #ebcb8b;
+}
+.radio-color.orange{
+    background-color: #d08770;
+}
+.wrapper {
+    width: 80%;
+    margin: auto;
+    max-width: 1440px;
+}
 .seating-container {
+    width: max-content;
+
     display: grid;
     grid-gap: 2px;
 }
 .seat {
     margin-bottom: 2px;
-    background-color: green;
+    background-color: #8fbcbb;
     height: 100%;
+    width: 20px;
+}
+.seat.fungible {
+    transform: scale(1.06);
+}
+.legend .seat {
+    display: inline-block;
+    margin-right: 1rem;
+    margin-bottom: 2px;
+    background-color: #8fbcbb;
+    height: 20px;
+    width: 20px;
 }
 .seat:hover {
     background-color: gray;
 }
 .seat.occupied {
-    background-color: darkgrey;
+    background-color: red;
 }
 .seat.selected {
-    background-color: darkolivegreen;
+    background-color: blue;
+}
+.seat.temporary-selected {
+    background-color: #88c0d0 !important;
 }
 
 </style>
